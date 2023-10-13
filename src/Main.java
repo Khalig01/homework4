7b001d5af6ff401d9d4e71c2c481b72378501238
Khal01 <aliyev_56@list.ru>
2023-10-14 00:57:16 +0300

    homework4

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+      int age=20 ;
+      if (age >= 18); {
+            System.out.println("Если возраст человека равен"+age+"то он совершеннолетний");
+        }
+        int ageTwo=16;
+        if (ageTwo <18);{
+
+            System.out.println("Если возраст человека равен"+ageTwo+"то он не достиг совершеннолетия,нужно немного подождать");
+        }
+        System.out.println("Задача 2");
+        int tempereture=4 ;
+        if(tempereture<5);{
+        System.out.println("На улице"+tempereture+"градусов нужно надеть шапку");
+        }
+        int temperetureTwo=6 ;
+        if(tempereture>5);{
+            System.out.println("На улице"+temperetureTwo+"градусов можно идти без шапки");
+        }
+
+
+
+
+        }
+    }
